ccb5ff63cddf50243c936fc2ff4b8e2207fdba01
朱宇 <i_zhuyu@cvte.com>
2020-05-09 08:24:31 +0800

    [fix]修复教师端bug

--- src/components/PaperDetail/createTitleItem/DeleteListItem.jsx
+++ src/components/PaperDetail/createTitleItem/DeleteListItem.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import {message} from 'antd';
+import {message,Modal} from 'antd';
 
 import {deleteTitle} from '../../../api/index'
 export default (props)=>{
     const {titleId,queryPaperDetail}=props;
+    
+    function onMadal(index){
+        return ()=>{
+            Modal.confirm({
+                content: "你确定是否删除吗",
+                okText: "删除",
+                cancelText: "取消",
+                onOk: deleteListItem(index)
+            })
+        }
+    }
     function deleteListItem(id){
         return ()=>{
           deleteTitle(id).then(res=>{
@@ -19,7 +30,7 @@ export default (props)=>{
     }
     return (
         <div>
-            <a key="list-loadmore-more" onClick={deleteListItem(titleId)}>删除</a>
+            <a key="list-loadmore-more" onClick={onMadal(titleId)}>删除</a>
         </div>
     )
 }
